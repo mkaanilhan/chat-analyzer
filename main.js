cdca3cc5a8b105ba447296b172b8b505ea28a74d
mkaanilhan <kaan.ilhan@proton.me>
2022-12-01 16:43:03 +0300

    changed object structure

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import moment from "moment";
 import { readFileSync } from "fs";
+import { addAbortSignal } from "stream";
 
 const file = readFileSync("_chat.txt", "utf8") // file should be added to root folder
 
 const rgxD = /\[([0-9]+(\.[0-9]+)+).*[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]{1,3})?\]/i;
-const rgxU = /] ([a-zA-ZiİçÇşŞğĞÜüÖö]+( [a-zA-ZiİçÇşŞğĞÜüÖö]+)+): /i;
+const rgxU = /] (.*?): /i
 
 const useRegex = (input, regex) => {
   return regex.test(input);
@@ -12,11 +13,19 @@ const useRegex = (input, regex) => {
 
 let textByLine = file.split("\n")
 
+const groupName = () => {
+
+  let name = textByLine[0].match(rgxU)[0]
+
+  return name.replace("] ", "").replace(": ", "")
+
+}
+
 const lineList = (textByLine, regex, format, replaceOne, replaceTwo) => {
 
   let list = []
 
-  textByLine.forEach((text) => {
+  textByLine.slice(3).forEach((text) => {
     if (useRegex(text, regex)) {
       let rxgValue = text.match(regex)[0]
       rxgValue = rxgValue.replace(replaceOne, "").replace(replaceTwo, "")
@@ -25,18 +34,32 @@ const lineList = (textByLine, regex, format, replaceOne, replaceTwo) => {
       list.push(formattedValue.isValid() ? item : rxgValue)
     }
   })
+
   return list
 }
 
 const mapper = (list) => {
+
   let map = {}
+
   list.sort().forEach((item) => {
     map[item] = map.hasOwnProperty(item) ? map[item] + 1 : 1
   })
+
   return map
 }
 
-console.log(mapper(lineList(textByLine, rgxD, "YYYY-MM", "]", "["))) //Month mapper
-console.log(mapper(lineList(textByLine, rgxD, "HH", "]", "["))) //Hour mapper
-console.log(mapper(lineList(textByLine, rgxU, "", "] ", ": "))) //Name mapper
+
+let mainData = {
+
+  groupName: groupName(),
+  numberOfMembers: Object.keys(mapper(lineList(textByLine, rgxU, "", "] ", ": "))).length,
+  byName: mapper(lineList(textByLine, rgxU, "", "] ", ": ")),
+  byHour: mapper(lineList(textByLine, rgxD, "HH", "]", "[")),
+  byMonth: mapper(lineList(textByLine, rgxD, "YYYY-MM", "]", "["))
+
+}
+
+console.log(mainData)
+
 
