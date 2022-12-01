b96f163367b09f89bcae03c7d8f43d243617c614
mkaanilhan <kaan.ilhan@proton.me>
2022-12-01 13:42:40 +0300

    merge different function to one

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-const fs = require("fs")
-const file = fs.readFileSync("_chat.txt", "utf8") // file should be added to root folder
+import moment from "moment";
+import { readFileSync } from "fs";
+
+const file = readFileSync("_chat.txt", "utf8") // file should be added to root folder
+
 const rgxD = /\[([0-9]+(\.[0-9]+)+).*[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]{1,3})?\]/i;
 const rgxU = /] ([a-zA-ZiİçÇşŞğĞÜüÖö]+( [a-zA-ZiİçÇşŞğĞÜüÖö]+)+): /i;
 
@@ -8,46 +11,32 @@ const useRegex = (input, regex) => {
 }
 
 let textByLine = file.split("\n")
-let dateList = [];
-let timeList = [];
-let namedList = [];
 
-textByLine.forEach((text) => {
-  if (useRegex(text, rgxD)) {
-    const dateTime = text.match(rgxD)[0]
-    const date = dateTime.substring(1, dateTime.length - 10)
-    const time = dateTime.substring(dateTime.length - 9, dateTime.length - 1)
-    dateList.push(date)
-    timeList.push(time)
-  }
-  if (useRegex(text, rgxU)) {
-    const k = text.match(rgxU)[0]
-    const l = k.substring(2, k.length - 2)
-    namedList.push(l)
-  }
-})
+const lineList = (textByLine, regex, format, replaceOne, replaceTwo) => {
 
-let nameMap = {}
+  let list = []
 
-namedList.forEach((item) => {
-  nameMap[item] = nameMap.hasOwnProperty(item) ? nameMap[item] + 1 : 1
-})
+  textByLine.forEach((text) => {
+    if (useRegex(text, regex)) {
+      let rxgValue = text.match(regex)[0]
+      rxgValue = rxgValue.replace(replaceOne, "").replace(replaceTwo, "")
+      const formattedValue = moment(rxgValue, "DD.MM.YYYY HH.mm.ss")
+      const item = formattedValue.format(format)
+      list.push(formattedValue.isValid() ? item : rxgValue)
+    }
+  })
+  return list
+}
 
+const mapper = (list) => {
+  let map = {}
+  list.sort().forEach((item) => {
+    map[item] = map.hasOwnProperty(item) ? map[item] + 1 : 1
+  })
+  return map
+}
 
-let dateMap = {}
-
-dateList.forEach((item) => {
-  const yearMonth = item.substring(item.length - 4, item.length) +
-    "-" + item.substring(item.length - 7, item.length - 5)
-  dateMap[yearMonth] = dateMap.hasOwnProperty(yearMonth) ? dateMap[yearMonth] + 1 : 1
-})
-
-let timeMap = {}
-
-timeList.sort().forEach((item) => {
-  const hour = item.substring(0, 2)
-  timeMap[hour] = timeMap.hasOwnProperty(hour) ? timeMap[hour] + 1 : 1
-})
-
-console.log(nameMap)
+console.log(mapper(lineList(textByLine, rgxD, "YYYY-MM", "]", "["))) //Month mapper
+console.log(mapper(lineList(textByLine, rgxD, "HH", "]", "["))) //Hour mapper
+console.log(mapper(lineList(textByLine, rgxU, "", "] ", ": "))) //Name mapper
 
